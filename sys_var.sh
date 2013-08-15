8d6566c2caaebf874501ee1eaaf6c36f7be66356
Your full name <hhuan10@gmail.com>
2013-08-15 11:58:01 +0800

    system virable.
    Change-Id: If124f843e6f76ac3e36a0bcd44839aee1c350ee6

---
+++ sys_var.sh
@@ -0,0 +1,13 @@
+# /bin/bash
+
+echo "This is a script example to illustrate how to use the system viarables"
+echo "BASH"     is $BASH
+echo "BASH_VERSION" is $BASH_VERSION
+echo "COLUMNS"  is $COLUMNS
+echo "LINES"    is $LINES
+echo "HOME"     is $HOME
+echo "LOGNAME"  is $LOGNAME
+echo "PATH"     is $PATH
+echo "USERNAME" is $USERNAME
+
+exit 0
